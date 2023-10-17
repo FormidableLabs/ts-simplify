d5c9c580812748b5f518f26d8458ff613ea89d6c
scottrippey <scott.william.rippey@gmail.com>
2023-10-17 10:35:24 -0600

    chore: initial typescript project template

---
+++ README.md
@@ -0,0 +1,16 @@
+# @TODO_TS_TEMPLATE
+
+TODO: SHORT DESCRIPTION
+
+## Install
+
+```sh
+yarn add @TODO_TS_TEMPLATE
+```
+
+## Usage
+
+```ts
+import { TODO } from '@TODO_TS_TEMPLATE';
+ 
+```
---
+++ src/index.test.ts
@@ -0,0 +1,11 @@
+import { beforeEach, describe, expect, test } from "vitest";
+
+beforeEach(() => {
+  //
+});
+
+describe("index", () => {
+  test("index", async () => {
+    expect("developer to write some tests").toEqual("TODO");
+  });
+});
