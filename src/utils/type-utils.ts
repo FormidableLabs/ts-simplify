2e3ed0754c812428804d4328d7e3275f5bdcc542
scottrippey <scott.william.rippey@gmail.com>
2023-10-18 08:14:25 -0600

    feature(cli): added copy of SimplifyDeep source

--- src/utils/type-utils.ts
+++ src/utils/type-utils.ts
@@ -5,3 +5,12 @@ export type DeepPartial<T> = T extends Array<infer TItem>
       [P in keyof T]?: DeepPartial<T[P]>;
     }
   : T;
+
+// A copy of this type is hard-coded in the `simplify-types.ts` code
+export type SimplifyDeep<T> = T extends (...args: infer A) => infer R
+  ? (...args: SimplifyDeep<A>) => SimplifyDeep<R>
+  : T extends object
+  ? T extends infer O
+    ? { [K in keyof O]: SimplifyDeep<O[K]> }
+    : never
+  : T;
