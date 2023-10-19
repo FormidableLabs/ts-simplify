2c845554c50a1e12500b70ad2a57a9f7f7581bec
scottrippey <scott.william.rippey@gmail.com>
2023-10-19 15:46:30 -0600

    feature(cli): upgraded CLI to yargs

--- src/bin/ts-simplify.ts
+++ src/bin/ts-simplify.ts
@@ -1,47 +1,55 @@
 #!/usr/bin/env node
 
 import fs from "node:fs";
-import { parse } from "ts-command-line-args";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 
 import { simplifyTypes } from "../simplify-types";
 
 const config = (() => {
-  const { source, output, overwrite } = parse(
-    {
-      source: {
-        alias: "s",
-        description:
-          "A TypeScript file that exports types.  The output will contain compiled versions of these exports.",
-        type: String,
-        defaultOption: true,
-        multiple: true,
-      },
-      output: {
-        alias: "o",
-        description: "A .d.ts file for outputting the compiled types.  If omitted, the file will be sent to stdout.",
-        type: String,
-        defaultValue: "",
-      },
-      overwrite: {
-        alias: "f",
-        description: "Overwrite the existing output file",
-        type: Boolean,
-        defaultValue: false,
-      },
-      help: { type: Boolean, defaultValue: false, alias: "h", description: "Prints this usage guide" },
-    },
-    {
-      helpArg: "help",
-    }
-  );
-  const sourceFile = source[0];
+  const result = yargs(hideBin(process.argv))
+    .scriptName("ts-simplify")
+    .usage("$0 <source.ts> [output.d.ts]")
+    .option("source", {
+      description: "A TypeScript file that exports types.  The output will contain compiled versions of these exports.",
+      alias: "s",
+      type: "string",
+    })
+    .option("output", {
+      description: "A .d.ts file for outputting the compiled types.  If omitted, the file will be sent to stdout.",
+      alias: "o",
+      type: "string",
+      defaultValue: "",
+    })
+    .option("overwrite", {
+      description: "Overwrite the existing output file",
+      alias: "f",
+      type: "boolean",
+      defaultValue: false,
+    })
+    .string("_")
+    .check((argv) => {
+      // Ensure we have a source:
+      if (!(argv.source || argv._[0])) {
+        throw new Error("Missing 'source' parameter ");
+      }
 
-  const outputFile = output || source[1] || "";
-  if (outputFile && !overwrite && fs.existsSync(outputFile)) {
-    console.error(`File '${outputFile}' already exists.  Use --overwrite (-f) to overwrite it.`);
-    process.exit(1);
-  }
-  return { outputFile, sourceFile };
+      // Check if output exists:
+      if (argv.output && !argv.overwrite && fs.existsSync(argv.output)) {
+        throw new Error(`File '${argv.output}' already exists.  Use --overwrite (-f) to overwrite it.`);
+      }
+
+      return true; // tell Yargs that the arguments passed the check
+    })
+    .help();
+
+  type Unwrap<T> = T extends Promise<infer Value> ? Value : T;
+  const argv = result.argv as Unwrap<typeof result.argv>;
+
+  return {
+    outputFile: argv.output || argv._[1],
+    sourceFile: argv.source || argv._[0],
+  };
 })();
 
 // Generate the simplified types:
@@ -49,7 +57,7 @@ const outputText = simplifyTypes({ sourceFile: config.sourceFile });
 // Output the results:
 if (config.outputFile) {
   fs.writeFileSync(config.outputFile, outputText);
-  console.log(`Created '${config.outputFile}'`);
+  console.log(`Output types to '${config.outputFile}'`);
 } else {
   console.log(outputText);
 }
